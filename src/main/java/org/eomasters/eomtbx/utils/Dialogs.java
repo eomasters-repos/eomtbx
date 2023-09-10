c4a67ba079fff10f1090416cb7e8a5be6fecd42b
Marco <info@eomasters.org>
2023-09-10 14:00:56 +0200

    added to Dialogs to ask for input

--- src/main/java/org/eomasters/eomtbx/utils/Dialogs.java
+++ src/main/java/org/eomasters/eomtbx/utils/Dialogs.java
@@ -9,12 +9,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * -> http://www.gnu.org/licenses/gpl-3.0.html
@@ -65,7 +65,7 @@ public final class Dialogs {
   /**
    * Shows an error dialog.
    *
-   * @param title   the title
+   * @param title        the title
    * @param errorMessage the error message
    */
   public static void error(String title, String errorMessage) {
@@ -91,6 +91,25 @@ public final class Dialogs {
     JOptionPane.showMessageDialog(null, messagePanel, title, JOptionPane.ERROR_MESSAGE);
   }
 
+  /**
+   * Shows a dialog asking the user for input.
+   *
+   * @param title  the title
+   * @param prompt the prompt
+   */
+  public static String input(String title, String prompt) {
+    return input(title, new JLabel(prompt));
+  }
+
+  /**
+   * Shows a prompt dialog.
+   *
+   * @param title  the title
+   * @param prompt the prompt component
+   */
+  public static String input(String title, JComponent prompt) {
+    return JOptionPane.showInputDialog(null, prompt, title, JOptionPane.QUESTION_MESSAGE);
+  }
 
   /**
    * Asks the user for confirmation.
