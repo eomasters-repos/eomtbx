0b97f0af54bce023b4528b68482f760db6f1658a
Marco <info@eomasters.org>
2023-11-10 10:43:37 +0100

    made fields private

--- src/main/java/org/eomasters/eomtbx/utils/CollapsiblePanel.java
+++ src/main/java/org/eomasters/eomtbx/utils/CollapsiblePanel.java
@@ -9,12 +9,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * -> http://www.gnu.org/licenses/gpl-3.0.html
@@ -49,8 +49,8 @@ import org.eomasters.eomtbx.EomToolbox;
  */
 public class CollapsiblePanel extends JPanel {
 
-  public static final String EXPAND_CHAR = "▶";
-  public static final String COLLAPSE_CHAR = "▼";
+  private static final String EXPAND_CHAR = "▶";
+  private static final String COLLAPSE_CHAR = "▼";
   private final JLabel toggleLabel;
   private final JComponent contentPanel;
   private final JPanel titlePanel;
