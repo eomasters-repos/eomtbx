bd762e7edb5483c27f7444cb8d05c1405cbf650d
Marco <info@eomasters.org>
2023-11-11 14:56:42 +0100

    fixed header

--- src/main/java/org/eomasters/eomtbx/utils/gui/PathEditor.java
+++ src/main/java/org/eomasters/eomtbx/utils/gui/PathEditor.java
@@ -9,33 +9,18 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * -> http://www.gnu.org/licenses/gpl-3.0.html
  * =========================LICENSE_END==================================
  */
 
-/*
- * Copyright (C) 2010 Brockmann Consult GmbH (info@brockmann-consult.de)
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 3 of the License, or (at your option)
- * any later version.
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, see http://www.gnu.org/licenses/
- */
 package org.eomasters.eomtbx.utils.gui;
 
 import com.bc.ceres.binding.PropertyDescriptor;
@@ -47,10 +32,10 @@ import java.nio.file.Path;
  */
 public class PathEditor extends FileEditor {
 
-    @Override
-    public boolean isValidFor(PropertyDescriptor propertyDescriptor) {
-        return Path.class.isAssignableFrom(propertyDescriptor.getType())
-               && !Boolean.TRUE.equals(propertyDescriptor.getAttribute("directory"));
-    }
+  @Override
+  public boolean isValidFor(PropertyDescriptor propertyDescriptor) {
+    return Path.class.isAssignableFrom(propertyDescriptor.getType())
+        && !Boolean.TRUE.equals(propertyDescriptor.getAttribute("directory"));
+  }
 
 }
