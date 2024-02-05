e6d5b282f4d91af2f1c26065753cb66c7cd7901f
Marco <info@eomasters.org>
2024-02-05 13:27:34 +0100

    added AboutBoxProvider

--- src/main/java/org/eomasters/eomtbx/AboutBoxProvider.java
+++ src/main/java/org/eomasters/eomtbx/AboutBoxProvider.java
@@ -9,12 +9,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * -> http://www.gnu.org/licenses/gpl-3.0.html
@@ -23,29 +23,12 @@
 
 package org.eomasters.eomtbx;
 
-import java.awt.BorderLayout;
-import java.util.ServiceLoader;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
-import javax.swing.border.EmptyBorder;
-import org.esa.snap.rcp.about.AboutBox;
+import java.awt.Component;
 
-@AboutBox(displayName = "EOMTBX", position = 500)
-public class EomtbxAboutBox extends JPanel {
+public interface AboutBoxProvider {
 
-  public EomtbxAboutBox() {
-    super(new BorderLayout(4, 4));
-    setBorder(new EmptyBorder(4, 4, 4, 4));
-    ServiceLoader<AboutBoxProvider> eomToolboxServiceLoader = ServiceLoader.load(AboutBoxProvider.class);
-    JTabbedPane jTabbedPane = new JTabbedPane();
-    eomToolboxServiceLoader.forEach(provider -> {
-      jTabbedPane.addTab(provider.getTitle(), provider.getAboutPanel());
-    });
 
-    JLabel label = new JLabel(new ImageIcon(EomtbxAboutBox.class.getResource("about-eomtbx-logo.png")));
-    add(label, BorderLayout.CENTER);
-    add(jTabbedPane, BorderLayout.SOUTH);
-  }
+  String getTitle();
+
+  Component getAboutPanel();
 }
---
+++ src/main/java/org/eomasters/eomtbx/FreeAboutBoxProvider.java
@@ -0,0 +1,72 @@
+/*-
+ * ========================LICENSE_START=================================
+ * EOMTBX - EOMasters Toolbox for SNAP
+ * -> https://www.eomasters.org/sw/EOMTBX
+ * ======================================================================
+ * Copyright (C) 2023 - 2024 Marco Peters
+ * ======================================================================
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * -> http://www.gnu.org/licenses/gpl-3.0.html
+ * =========================LICENSE_END==================================
+ */
+
+package org.eomasters.eomtbx;
+
+import java.awt.Component;
+import java.awt.Cursor;
+import java.net.URI;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import org.esa.snap.rcp.util.BrowserUtils;
+import org.openide.modules.ModuleInfo;
+import org.openide.modules.Modules;
+
+public class FreeAboutBoxProvider implements AboutBoxProvider {
+
+  public FreeAboutBoxProvider() {
+  }
+
+  @Override
+  public String getTitle() {
+    return "Free";
+  }
+
+  @Override
+  public Component getAboutPanel() {
+    String eomLink = "<a href=\"" + EomToolbox.EOMASTERS_URL + "\">EOMasters</a>";
+    JLabel copyrightLabel = new JLabel("<html><b>©2023-2024 Marco Peters from " + eomLink + "</b>",
+        SwingConstants.CENTER);
+    copyrightLabel.addMouseListener(new BrowserUtils.URLClickAdaptor(EomToolbox.EOMASTERS_URL.toString()));
+
+    final ModuleInfo moduleInfo = Modules.getDefault().ownerOf(getClass());
+    JLabel versionLabel = new JLabel(
+        "<html><b>EOMasters Toolbox Free version " + moduleInfo.getImplementationVersion() + "</b>",
+        SwingConstants.CENTER);
+
+    URI changelogUri = URI.create("https://github.com/eomasters-repos/eomtbx/releases");
+    final JLabel releaseNoteLabel = new JLabel("<html><a href=\"" + changelogUri + "\">Release Notes</a>",
+        SwingConstants.CENTER);
+    releaseNoteLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+    releaseNoteLabel.addMouseListener(new BrowserUtils.URLClickAdaptor(changelogUri.toString()));
+
+    final JPanel mainPanel = new JPanel();
+    mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+    mainPanel.add(copyrightLabel);
+    mainPanel.add(versionLabel);
+    mainPanel.add(releaseNoteLabel);
+    return mainPanel;
+  }
+}
