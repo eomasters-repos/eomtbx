362a9aa87f7050b9629dcf64aeec5f2e4f8d5701
Marco <info@eomasters.org>
2024-02-05 09:34:49 +0100

    updated header

--- src/main/java/org/eomasters/eomtbx/preferences/package-info.java
+++ src/main/java/org/eomasters/eomtbx/preferences/package-info.java
@@ -3,7 +3,7 @@
  * EOMTBX PRO - EOMasters Toolbox PRO for SNAP
  * -> https://www.eomasters.org/sw/EOMTBX
  * ======================================================================
- * Copyright (C) 2023 Marco Peters
+ * Copyright (C) 2023 - 2024 Marco Peters
  * ======================================================================
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
